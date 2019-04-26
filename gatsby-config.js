3d211aa2252d94a265a5ed5053de4a6fc6cbb384
dominait <imailjim@gmail.com>
2019-04-26 13:22:38 -0600

    init commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,15 +27,15 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // {
-    //   resolve: `gatsby-source-wordpress`,
-    //   options: {
-    //     baseUrl: `www.growforthecure.com`,
-    //     protocol: `https`,
-    //     hostingWPCOM: false,
-    //     useACF: true,
-    //   },
-    // },
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        baseUrl: `www.growforthecure.com`,
+        protocol: `https`,
+        hostingWPCOM: false,
+        useACF: true,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
