67fe4f8e8ad7b220b1bf22ab8cc77bc267b9fd78
dominait <imailjim@gmail.com>
2019-04-26 13:18:21 -0600

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from "react"
 
 const IndexPage = () => {
+  let date = new Date()
+  let newDate = date.getSeconds()
   return (
     <div>
-      <h1>Shit Fuck Piss</h1>
+      <h1>{newDate}</h1>
       <p>{`The key is: ${process.env.GATSBY_SITE}`}</p>
       <p>{`The key is: ${process.env.GATSBY_USER}`}</p>
       <p>{`The key is: ${process.env.GATSBY_PASSWORD}`}</p>
